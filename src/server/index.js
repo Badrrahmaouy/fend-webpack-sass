e778dd7125b16671c1100953b98533853452b03e
Badr Rahmaouy <rahmaouy@gmail.com>
2021-03-05 15:54:03 +0100

    Instatiated post request from express server for the API

--- src/server/index.js
+++ src/server/index.js
@@ -1,15 +1,41 @@
 var path = require('path')
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const cors = require('cors')
+// const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv')
+const fetch = require('fetch')
+const bodyParser = require('body-parser')
+
+// inizialize API key
+dotenv.config()
+console.log(`Your API Key is ${process.dotenv.API_KEY}`)
 
 const app = express()
-
-const cors = require('cors')
 app.use(cors())
-
 app.use(express.static('dist'))
-
 console.log(__dirname)
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+// get text submitted in the page
+const input = {}
+app.post('/data', function(req, res) {
+    input.data = req.body
+    console.log(input)
+    res.send(input)
+})
+
+// send data received from the page to the API
+const apiURL = `https://api.meaningcloud.com/sentiment-2.1?key=${process.dotenv.API_KEY}&of=json&txt=${input.data}&lang=en`
+fetch(apiURL, {
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: {
+        'Content-Type': 'application/json'
+    } 
+}) .catch (err => console.log('error', err))
+.then(res => res.json())
+.then(json => console.log(json))
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
@@ -20,6 +46,6 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+// app.get('/test', function (req, res) {
+//     res.send(mockAPIResponse)
+// })
